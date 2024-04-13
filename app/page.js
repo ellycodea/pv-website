7e08acb9942607fa36461aa124308e84b71dfeb7
unknown <machadomarcos2019@gmail.com>
2024-04-13 16:29:17 -0400

    polar and finn

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <NavBar />
       <main className="pt-20 overflow-y-auto">
       <div className="bg-black w-full mt-4 p-4 flex flex-row ">
-         <Image src="/banner.webp" width={700} height={700} className="rounded-md"/>
+            <Image src="/banner.webp" width={700} height={700} className="rounded-md"/>
          <div className="ml-8 flex flex-col">
             <p className="font-bold text-4xl">Descobrindo novos mundos!</p>
             <p className="text-slate-500 mt-4">Olá viajante! Você acaba de entrar no site oficial da Perfect Visions, um grupo de desenvolvedores que estão constantemente construindo novos projetos significativos na comunidade de developers, a Perfect Visions inicialmente foi criada para ser o orgão por trás do bot <Link href="https://discord.gg/mQgFvnUaUJ" className="text-blue-500 hover:text-blue-600">Jujuba</Link>, porém ao longo do temponos tornamos um grupo imenso com diversos projetos que de alguma forma significativa ajuda a comunidade de developers em geral.</p>
--- app/projects/page.js
+++ app/projects/page.js
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <main className="pt-20 overflow-y-auto">
-      <div className="bg-black w-full mt-4 p-4 flex flex-row ">
-        <Link href="https://discord.gg/mQgFvnUaUJ" className="p-2 rounded flex flex-row max-w-2xl w-full">
+      <main className="pt-16 overflow-y-auto">
+      <div className="bg-black w-full p-4 flex flex-col items-center justify-center ">
+        <Link href="https://discord.gg/mQgFvnUaUJ" className="p-2 rounded flex flex-row max-w-2xl w-full hover:bg-slate-900 ">
             <Image src="/ju-icon.png" width={120} height={120} className="rounded-md hover:opacity-70 border-2 border-pink-500"></Image>
             <div className="ml-4">
             <p className="font-bold flex flex-row items-center gap-2 mt-2">Jujuba <MdVerified /></p>
@@ -17,11 +17,27 @@ export default function Home() {
        
             </div>
         </Link>
-        <Link href="https://discord.gg/2uGFtGfpP6" className="p-2 rounded flex flex-row max-w-2xl w-full">
+        <Link href="https://discord.gg/2uGFtGfpP6" className="p-2 rounded flex flex-row max-w-2xl w-full hover:bg-slate-900">
             <Image src="/luckky.png" width={120} height={120} className="rounded-md hover:opacity-70 border-2 border-yellow-500"></Image>
             <div className="ml-4">
             <p className="font-bold flex flex-row items-center gap-2 mt-2">Luckky <MdVerified /></p>
             <p className="mt-2 text-slate-300">A Luckky. é uma loja para quem gosta de algo que misture emoções e cores, pode escolher entre algo sério, ou algo divertido. O nosso objetivo é mostrar a melhor face do seu produto, criada em agosto de 2022 por liftzera.</p>
+       
+            </div>
+        </Link>
+        <Link href="https://discord.gg/2WckKPtG" className="p-2 rounded flex flex-row max-w-2xl w-full hover:bg-slate-900">
+            <Image src="/finn.png" width={120} height={120} className="rounded-md hover:opacity-70 border-2 border-blue-500"></Image>
+            <div className="ml-4">
+            <p className="font-bold flex flex-row items-center gap-2 mt-2">Finn Bot</p>
+            <p className="mt-2 text-slate-300">O Finn, e um bot para auxiliar seu servidor, com sistemas de moderação, diversão e muito mais, trazendo inovação para o seu servidor, criado em 2024 por imdavixz.</p>
+       
+            </div>
+        </Link>
+        <Link href="https://discord.com/invite/W5cxqvXwmy" className="p-2 rounded flex flex-row max-w-2xl w-full hover:bg-slate-900">
+            <Image src="/polar.webp" width={120} height={120} className="rounded-md hover:opacity-70 border-2 border-blue-500"></Image>
+            <div className="ml-4">
+            <p className="font-bold flex flex-row items-center gap-2 mt-2">Polar</p>
+            <p className="mt-2 text-slate-300">Polar é um bot feito para Discord com sistemas incríveis que deixaram deu servidor cada vez melhor e único, criado em 2024 por shinra2504k</p>
        
             </div>
         </Link>
