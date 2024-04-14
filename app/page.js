cd710fe627608a3230180ad9b49c6a64b25c2bc3
unknown <machadomarcos2019@gmail.com>
2024-04-14 07:50:31 -0400

    ads txt

--- app/page.js
+++ app/page.js
@@ -77,43 +77,42 @@ export default function Home() {
     setLoading(false)
   }, []);
   return (
-    <><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2048560139347623"
-      crossorigin="anonymous"></script><div className="flex flex-col">
-        {isLoading ? <div className="w-screen h-screen bg-black fixed flex items-center justify-center content-center">
-          <LuLoader2 className="animate-spin" />
-        </div> : ''}
-        <NavBar />
-        <main className="pt-20 overflow-y-auto ">
-          <div className="bg-black w-full mt-4 p-4 flex flex-row">
-            {isMobile ? '' : <Image draggable={false} src="/banner.webp" width={700} height={700} className="rounded-md" />}
-            <div className={isMobile ? "flex flex-col justify-center" : "ml-8 flex flex-col justify-center"}>
-              <p className="font-bold text-4xl">Descobrindo novos mundos!</p>
-              <p className="text-slate-500 mt-4 w-full">Olá viajante! Você acaba de entrar no site oficial da Perfect Visions, um grupo de desenvolvedores que estão constantemente construindo novos projetos significativos na comunidade de developers, a Perfect Visions inicialmente foi criada para ser o orgão por trás do bot <Link href="https://discord.gg/mQgFvnUaUJ" className="text-blue-500 hover:text-blue-600">Jujuba</Link>, porém ao longo do tempo nos tornamos um grupo imenso com diversos projetos que de alguma forma significativa ajuda a comunidade de developers em geral.</p>
-              <div className="flex flex-row gap-4 w-full">
-                <Link href="https://discord.gg/mQgFvnUaUJ" className="bg-yellow-500 p-2 rounded w-full max-w-32 hover:bg-yellow-600 items-center flex content-center justify-center font-bold mt-4 gap-2">Descubra <FiExternalLink /></Link>
-                <Link href="https://www.instagram.com/perfectvisions_oficial/" className="bg-pink-600 p-2 rounded w-full max-w-32 hover:bg-pink-700 items-center flex content-center justify-center font-bold mt-4 gap-2">Instagram <FiExternalLink /></Link>
-                <Link href="https://linkai.social/@PerfectVisions" className="bg-green-500 p-2 rounded  w-full max-w-32 hover:bg-green-700 items-center flex content-center justify-center font-bold mt-4 gap-2">Linkai <FiExternalLink /></Link>
-              </div>
+    <div className="flex flex-col">
+      {isLoading ? <div className="w-screen h-screen bg-black fixed flex items-center justify-center content-center">
+        <LuLoader2 className="animate-spin"/>
+      </div> : ''}
+      <NavBar />
+      <main className="pt-20 overflow-y-auto ">
+        <div className="bg-black w-full mt-4 p-4 flex flex-row">
+         {isMobile ? '' :  <Image draggable={false} src="/banner.webp" width={700} height={700} className="rounded-md" />}
+          <div className={isMobile ? "flex flex-col justify-center" : "ml-8 flex flex-col justify-center"}>
+            <p className="font-bold text-4xl">Descobrindo novos mundos!</p>
+            <p className="text-slate-500 mt-4 w-full">Olá viajante! Você acaba de entrar no site oficial da Perfect Visions, um grupo de desenvolvedores que estão constantemente construindo novos projetos significativos na comunidade de developers, a Perfect Visions inicialmente foi criada para ser o orgão por trás do bot <Link href="https://discord.gg/mQgFvnUaUJ" className="text-blue-500 hover:text-blue-600">Jujuba</Link>, porém ao longo do tempo nos tornamos um grupo imenso com diversos projetos que de alguma forma significativa ajuda a comunidade de developers em geral.</p>
+            <div className="flex flex-row gap-4 w-full">
+              <Link href="https://discord.gg/mQgFvnUaUJ" className="bg-yellow-500 p-2 rounded w-full max-w-32 hover:bg-yellow-600 items-center flex content-center justify-center font-bold mt-4 gap-2">Descubra <FiExternalLink /></Link>
+              <Link href="https://www.instagram.com/perfectvisions_oficial/" className="bg-pink-600 p-2 rounded w-full max-w-32 hover:bg-pink-700 items-center flex content-center justify-center font-bold mt-4 gap-2">Instagram <FiExternalLink /></Link>
+              <Link href="https://linkai.social/@PerfectVisions" className="bg-green-500 p-2 rounded  w-full max-w-32 hover:bg-green-700 items-center flex content-center justify-center font-bold mt-4 gap-2">Linkai <FiExternalLink /></Link>
             </div>
           </div>
-          <p className="m-2 ml-6 font-bold">Featured</p>
-          <div className="flex flex-row">
-            <button onClick={onPrev}><FaAngleLeft /></button>
-            <div className="overflow-x-auto scrollbar-container horizontal-scroll-wrapper" ref={slideRef}>
-              <div id="newswire-content" className="flex flex-nowrap w-fit gap-2 p-2 scrollbar-content">
-                {ar.map(a => (
-                  <Link title={a.username} href={a.link} className="hover:opacity-50 w-full min-w-24 max-w-52  bg-black rounded-lg flex flex-col border-[0.5px] border-slate-700 hover:bg-slate-900 cursor-pointer">
-                    <Image alt="Newswire Perfect Visions" key={a.username} draggable={false} src={a.image} className="rounded-lg w-full h-full" width={200} height={500} />
-
-                  </Link>
-                ))}
-              </div>
+        </div>
+        <p className="m-2 ml-6 font-bold">Featured</p>
+        <div className="flex flex-row">
+          <button onClick={onPrev}><FaAngleLeft /></button>
+          <div className="overflow-x-auto scrollbar-container horizontal-scroll-wrapper" ref={slideRef}>
+            <div id="newswire-content" className="flex flex-nowrap w-fit gap-2 p-2 scrollbar-content">
+              {ar.map(a => (
+                <Link title={a.username} href={a.link} className="hover:opacity-50 w-full min-w-24 max-w-52  bg-black rounded-lg flex flex-col border-[0.5px] border-slate-700 hover:bg-slate-900 cursor-pointer">
+                  <Image alt="Newswire Perfect Visions" key={a.username} draggable={false} src={a.image} className="rounded-lg w-full h-full" width={200} height={500} />
+                  
+                </Link>
+              ))}
             </div>
-            <button onClick={onNext} className="ml-auto"><FaAngleRight /></button>
           </div>
-        </main>
-        <Footer />
-
-      </div></>
+          <button onClick={onNext} className="ml-auto"><FaAngleRight /></button>
+        </div>
+      </main>
+      <Footer/>
+      
+    </div>
   );
 }
